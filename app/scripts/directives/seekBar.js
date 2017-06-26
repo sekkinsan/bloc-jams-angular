057ef1341e7b22c377caa641085da5f443f26dbe
JacobYun <jacob.yun429@gmail.com>
2017-06-25 17:00:06 -0700

    completed checkpoint-10

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -17,7 +17,9 @@
             //specifies what the template should replace (true = replace directive's element; false = replace contents of directive's element)
             restrict: 'E',
             //restricts the directive to a specific declaration style
-            scope: { },
+            scope: {
+                  onChange: '&'
+                },
             //specifies that a new scope be created for the directive
             link: function(scope, element, attributes) {
             //responsible for registering DOM listeners and updating the DOM
@@ -27,11 +29,25 @@
 
                 var seekBar = $(element); //holds the element that matches directive <seek-bar> as a jquery object so we can call jquery methods on it
 
+                attributes.$observe('value', function(newValue) {
+                    scope.value = newValue;
+                });
+
+                attributes.$observe('max', function(newValue) {
+                    scope.max = newValue;
+                });
+
                 var percentString = function () { //function that calculates a percent based on value and max value of seek bar
                     var value = scope.value;
                     var max = scope.max;
                     var percent = value / max * 100;
                     return percent + "%";
+                };
+
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
                 };
 
                 scope.fillStyle = function() { //returns the width of the seek bar fill element based on calculated percent
@@ -45,6 +61,7 @@
                 scope.onClickSeekBar = function(event) { //updates the seek bar value based on seek bar's width and location of user's click on seek bar
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
 
                 scope.trackThumb = function() {
@@ -52,6 +69,7 @@
                         var percent = calculatePercent(seekBar, event);
                         scope.$apply(function() {
                             scope.value = percent * scope.max;
+                            notifyOnChange(scope.value);
                         });
                     });
 
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function () {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
 
         var currentAlbum = Fixtures.getAlbum();
@@ -24,6 +24,12 @@
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
+            });
+
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
             });
 
             SongPlayer.currentSong = song;
@@ -53,6 +59,18 @@
          * @type {Object}
          */
         SongPlayer.currentSong = null;
+
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
+
+        /**
+        * @desc Volume of song
+        * @type {Number}
+        */
+        SongPlayer.volume = 50;
 
 
         /**
@@ -111,10 +129,31 @@
             }
         };
 
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                  currentBuzzObject.setTime(time);
+            }
+        };
+
+        SongPlayer.setVolume = function(volume) {
+            if (currentBuzzObject) {
+                  currentBuzzObject.setVolume(volume);
+            }
+            SongPlayer.volume = volume;
+        };
+
+
+
         return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
